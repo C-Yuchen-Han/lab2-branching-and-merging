e3288786c98e716271c9b3506d62b528f4696db3
C-Yuchen-Han <cathy.han@mail.utoronto.ca>
2026-09-21 16:24:35 -0400

    Fixed the bugs 2

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,8 +1,8 @@
 public class DataTypes {
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
-        for (long x : numbers) {
+        for (int x : numbers) {
             s += x;
         }
         return s;
